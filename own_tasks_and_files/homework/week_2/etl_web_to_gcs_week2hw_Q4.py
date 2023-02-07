304649ded2cad0621fba91d95d215b746ff2ae77
dimkaart <dmitri.artjuch@tu-dortmund.de>
2023-02-07 20:37:12 +0100

    test

--- own_tasks_and_files/homework/week_2/etl_web_to_gcs_week2hw_Q4.py
+++ own_tasks_and_files/homework/week_2/etl_web_to_gcs_week2hw_Q4.py
@@ -53,7 +53,7 @@ def write_gcs(path: Path) -> None:
 def etl_web_to_gcs() -> None:
     """ The main ETL function"""
     color = "green"
-    year = 2019
+    year = 2020
     month = 11
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
