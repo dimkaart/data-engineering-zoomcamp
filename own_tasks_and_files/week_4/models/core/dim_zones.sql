eb42fed5844a02b3b2c707f2c25a6e7e3fa6edce
Dmitri <56168624+dimkaart@users.noreply.github.com>
2023-03-06 21:09:18 +0100

    Delete own_tasks_and_files/week_4 directory

--- own_tasks_and_files/week_4/models/core/dim_zones.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT 
-    locationid,
-    borough,
-    zone,
-    replace(service_zone, 'Boro','Green') as service_zone
-
-FROM {{ ref('taxi_zone_lookup') }}
